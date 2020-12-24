ee113e79bdf6e258648530565708b0064d11fc2d
Mustafa Tok <67019402+tokmustafa@users.noreply.github.com>
2020-12-23 20:03:54 -0600

    Update main.tf

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,5 +1,5 @@
 module "academy-deploy" {
-  source  = "fuchicorp/chart/helm"
+  source  = "tuyalou/chart/helm"
   deployment_name        = "hello-world"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
